4968a270f214ae0df1eb228c0d630b8ac5451aff
Henrique Paixao <henriquemfpaixao@gmail.com>
2024-04-02 20:41:31 -0300

    comit aula 1

---
+++ app.js
@@ -0,0 +1,13 @@
+<script type="module">
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDxUt9uPiV4DChGlY_rX1i4aBwNHNK8VUE",
+    authDomain: "app-libri-henrique.firebaseapp.com",
+    projectId: "app-libri-henrique",
+    storageBucket: "app-libri-henrique.appspot.com",
+    messagingSenderId: "892303643855",
+    appId: "1:892303643855:web:0bdd3b1e720764298793ba",
+    measurementId: "G-X59EM8GSZS"
+  };
+
+</script>
