d4f3547523af6321ef937803344ccd92e6e95956
sr-paixao <henriquemfpaixao@gmail.com>
2024-04-16 20:31:13 -0300

    insert e leitura banco

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
-<script type="module">
+const livrolist = document.querySelector('#book-list')
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyDxUt9uPiV4DChGlY_rX1i4aBwNHNK8VUE",
-    authDomain: "app-libri-henrique.firebaseapp.com",
-    projectId: "app-libri-henrique",
-    storageBucket: "app-libri-henrique.appspot.com",
-    messagingSenderId: "892303643855",
-    appId: "1:892303643855:web:0bdd3b1e720764298793ba",
-    measurementId: "G-X59EM8GSZS"
-  };
+function renderBook(doc) {
+    //criaçao elementos html
+    let li = document.createElement('li')
+    let titulo = document.createElement('span')
+    let autor = document.createElement('span')
 
-</script>
+    //carrega os dados nos elementos html
+
+    li.setAttribute('data-id', doc.id)
+    titulo.textContent = doc.data().titulo
+    autor.textContent = doc.data().autor
+
+    //adicionando dados de autor e titulo na tag li
+    li.appendChild(titulo)
+    li.appendChild(autor)
+
+    //adicionando o li na tag ul
+    livrolist.appendChild(li)
+    
+}
+
+
+
+db.collection('libri-firestore')
+    .get()
+    .then(
+        (snapshot) => {
+            // console.log(snapshot.docs)
+            snapshot.docs.forEach(doc => {
+                console.log(doc.data())
+                renderBook(doc)      
+            });
+        }
+    )
+
+
+    //insert de livros e autores
+    const form = document.querySelector('#add-book-form')
+
+    form.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        db.collection('libri-firestore').add({
+            autor: form.autor.value,
+            titulo: form.titulo.value
+        })
+        .then(()=>{
+            form.autor.value = '';
+            form.titulo.value = '';
+            window.location.reload();
+        })
+    });
+
+
+
